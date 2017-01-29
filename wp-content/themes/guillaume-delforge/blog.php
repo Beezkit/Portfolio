5c0e2f4f58e47a3583e435d183bc13de03f112aa
Beezkit <g.delforge.gd@gmail.com>
2017-01-29 09:06:22 +0100

    creation blog

--- wp-content/themes/guillaume-delforge/blog.php
+++ wp-content/themes/guillaume-delforge/blog.php
@@ -14,16 +14,67 @@
                 <?php custom_breadcrumbs(); ?>
             </div>
         </div>
-        <h1><?php the_title(); ?></h1>
-<?php query_posts( array ( 'post_type' => 'blog', 'order' => 'ASC' ) ); ?>
+        <div class="xs-grid-12">
+            <div class="text-center">
+                <h1><?php the_title(); ?></h1>
+            </div>
+            <?php query_posts( array ( 'post_type' => 'blog', 'order' => 'ASC' ) ); ?>
 
-    <?php while ( have_posts() ) : the_post(); ?>
+            <?php
+			//list terms in a given taxonomy (useful as a widget for twentyten)
+			$taxonomy = 'catégories';
+			$tax_terms = get_terms($taxonomy);
+			?>
+            <div class="filter">
+                <div class="icon">
+                    <span>Filtre</span>
+                    <i class="fa fa-filter" aria-hidden="true"></i>
+                </div>
+                <?php $taxonomy = 'catégories';
 
-        <h2><?php the_title(); ?></h2>
+// Get the term IDs assigned to post.
+$post_terms = wp_get_object_terms( $post->ID, $taxonomy, array( 'fields' => 'ids' ) );
 
-    <?php endwhile; ?>
+// Separator between links.
+$separator = ', ';
 
-<?php wp_reset_query(); ?>
+if ( ! empty( $post_terms ) && ! is_wp_error( $post_terms ) ) {
+
+    $term_ids = implode( ',' , $post_terms );
+
+    $terms = wp_list_categories( array(
+        'title_li' => '',
+        'style'    => 'none',
+        'echo'     => false,
+        'taxonomy' => $taxonomy,
+        'include'  => $term_ids
+    ) );
+
+    $terms = rtrim( trim( str_replace( '<br />',  $separator, $terms ) ), $separator );
+
+    // Display post categories.
+    echo  $terms;
+}?>
+    			<ul>
+    				<?php
+                    var_dump($tax_terms);
+    				foreach ($tax_terms as $tax_term) {
+    				echo '<li class="category" data-ref="'.$tax_term->name.'">' . $tax_term->name. '</li>';
+    				}
+    				?>
+    				<li class="category" data-ref="all">Tout</li>
+    			</ul>
+            </div>
+
+                <?php while ( have_posts() ) : the_post(); ?>
+
+                    <h2><?php the_title(); ?></h2>
+
+                <?php endwhile; ?>
+
+            <?php wp_reset_query(); ?>
+        </div>
+        <div class="clear"></div>
     </div>
 </div>
 
